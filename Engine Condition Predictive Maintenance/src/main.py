f2cf0a7190b3c6f34be73594876d7fa3a175917c
codewithraphael <raphaeldaniel223@gmail.com>
2026-06-24 00:39:05 +0100

    added feature engineering

--- Engine Condition Predictive Maintenance/src/main.py
+++ Engine Condition Predictive Maintenance/src/main.py
@@ -65,10 +65,81 @@ def plot_target(data):
 #  PAIRPLOR DISTRIBUTION
 # =======================
 
-def pairplot(data):
+def plot_pairplot(data):
     sns.pairplot(data)
     plt.savefig('../plots/pairplot_distribution.png')
-    plt.close()    
+    plt.close()
+
+
+
+# ====================
+#  FEATURE ENGINEERING
+# ====================
+
+def feature_enginneering(data):
+
+    # total pressure
+    data['total_pressure'] = data['Lub oil pressure'] + data['Fuel pressure'] + data['Coolant pressure']
+
+    # total temperature
+    data['total_temp'] = data['lub oil temp'] + data['Coolant temp']
+
+    # temperature difference
+    data['temp_difference'] = data['lub oil temp'] - data['Coolant temp']
+    data['pressure_difference'] = data['Lub oil pressure'] - data['Coolant pressure']
+    
+    # pressure difference
+    data['temp_ratio'] = data['lub oil temp'] / data['Coolant temp']
+    data['pressure_ratio'] = data['Lub oil pressure'] / data['Coolant temp']
+    
+    # rpm interaction
+    data['rpm_oil_temp'] = data['Engine rpm'] * data['lub oil temp']
+    data['rpm_coolant_temp'] = data['Coolant temp'] * data['Engine rpm']
+    data['coolant_temp_per_rpm'] = data['Coolant temp'] / data['Engine rpm']
+    data['oil_temp_per_rpm'] = data['lub oil temp'] / data['Engine rpm']
+
+    # temperature categories
+    data["oil_temp_category"] = pd.cut(
+    data["lub oil temp"],
+    bins=3,
+    labels=["Low", "Normal", "High"])
+
+    # pressure categories
+    data["oil_pressure_status"] = pd.cut(
+    data["Lub oil pressure"],
+    bins=3,
+    labels=["Low", "Normal", "High"])
+
+    # Engine Load Score
+    data['engine_load'] = ( 
+        data['Engine rpm'] *
+        data['Fuel pressure'] * 
+        data['Coolant temp']
+        ) / data['Lub oil pressure'].replace(0, np.nan)
+
+    # load categories
+    data["load_level"] = pd.cut(
+    data["engine_load"],
+    bins=3,
+    labels=["Low", "Normal", "High"])
+
+
+    return data
+
+
+# ====================
+#  CORRELATION HEATMAP  
+# ====================
+
+def plot_correlation(data):
+    corr_matrix = data.select_dtypes(include=['number']).corr()
+
+    plt.figure(figsize=(20, 20))
+    sns.heatmap(corr_matrix, annot=True, linewidths=0.5, cmap='viridis')
+    plt.title('Features Correlation Heatmap')
+    plt.savefig('../plots/features_correlation_plot.png')
+    plt.close()
+
 
 # ====================
 #  MAIN FUNCTION
@@ -80,7 +151,10 @@ def main():
 
     eda(data)
     plot_target(data)
-    pairplot(data)
+    plot_pairplot(data)
+
+    feature_enginneering(data)
+    plot_correlation(data)
 
 
 if __name__ == "__main__":
