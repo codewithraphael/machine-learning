3996a223d554fc848095f9bc648896440c299237
codewithraphael <raphaeldaniel223@gmail.com>
2026-07-04 10:43:06 +0100

    configuration file

---
+++ Synthetic Heart Attack/src/config.py
@@ -0,0 +1,21 @@
+target_column = 'heart_attack'
+
+drop_column = 'patient_id'
+
+numeric_features = [
+    'age',
+    'resting_bp',
+    'cholestrol',
+    'fasting_bs',
+    'max_heart_rate',
+    'st_depression',
+    'num_major_vessels'
+]
+
+categorical_features = [
+    'sex',
+    'ecg_result',
+    'exercise_angina'
+    'slope',
+    'thalassemia'
+]
