d9e1e1f47269e03d60fbb0e4f4c7f0b44854220e
codewithraphael <raphaeldaniel223@gmail.com>
2026-07-07 22:45:39 +0100

    add FASTAPI endpoint for prediction testing

--- Concrete Compressive Strength/api/concrete_strength.py
+++ Concrete Compressive Strength/api/concrete_strength.py
@@ -1,1 +1,35 @@
-import pandas 
+import pandas as pd
+
+import joblib
+import  uvicorn
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+model = joblib.load('../models/xgboost_model.joblib')
+
+class ConcreteStrength(BaseModel):
+    cement: float
+    blast_furnace_slag: float
+    fly_ash: float
+    water: float
+    superplasticizer: float
+    coarse_aggregate: float
+    fine_aggregate: float
+    age: int
+
+
+@app.get('/')
+def root():
+    return {'message': 'concrete strength prediction api is live'}
+
+
+@app.post('/predict')
+def predict(data: ConcreteStrength):
+    input_dict = data.model_dump()
+    input_data = pd.DataFrame([input_dict])
+    prediction = model.predict(input_data)
+    
+    return {'predicted_strength': prediction[0]}
