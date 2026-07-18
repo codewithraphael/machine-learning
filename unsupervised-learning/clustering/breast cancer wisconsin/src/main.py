4d40bf70fdb4148ecd1b1010b317157e77cfed05
codewithraphael <raphaeldaniel223@gmail.com>
2026-07-18 15:27:45 +0100

    add: import TSNE for dimensionality reduction in clustering analysis

--- unsupervised-learning/clustering/breast cancer wisconsin/src/main.py
+++ unsupervised-learning/clustering/breast cancer wisconsin/src/main.py
@@ -11,6 +11,7 @@ from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 
 from sklearn.metrics import silhouette_score
+from sklearn.manifold import TSNE
 
 import warnings
 warnings.filterwarnings('ignore')
