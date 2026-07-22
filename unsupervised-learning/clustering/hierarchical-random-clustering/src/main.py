08fbabf4f7670e76ddba9d9f952483d8fa2c54e8
codewithraphael <raphaeldaniel223@gmail.com>
2026-07-22 21:22:15 +0100

    add: implement row clustering computation using hierarchical linkage

--- unsupervised-learning/clustering/hierarchical-random-clustering/src/main.py
+++ unsupervised-learning/clustering/hierarchical-random-clustering/src/main.py
@@ -45,6 +45,26 @@ def calculate_distance(data, labels):
 
 
 
+# =========================
+#  COMPUTING ROW CLUSTERS
+# =========================
+
+def compute_row_clusters(data, row_dist):
+
+    row_clusters = linkage(pdist(
+        data,
+        method='complete',
+        metric='euclidean')
+    )
+
+    pd.DataFrame(row_clusters, 
+                 columns=['row label 1',
+                          'row label 2',
+                          'distance',
+                          'no. of items in clust.'], 
+                          index=[f'cluster {(i + 1)}' for i in range (row_clusters.shape[0])])
+
+
 
 
 
@@ -58,7 +78,7 @@ def calculate_distance(data, labels):
 def main():
     data, labels = generate_data()
     row_dist = calculate_distance(data, labels)
-
+    compute_row_clusters(data, row_dist)
 
 
 if __name__ == '__main__':
