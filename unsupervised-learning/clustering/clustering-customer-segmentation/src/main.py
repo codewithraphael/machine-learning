2999fbe3200e73ece8854373ba207aac6869177b
codewithraphael <raphaeldaniel223@gmail.com>
2026-07-26 15:55:06 +0100

    add: implement initial customer segmentation analysis with data loading and EDA functions

---
+++ unsupervised-learning/clustering/clustering-customer-segmentation/src/main.py
@@ -0,0 +1,89 @@
+import pandas as pd
+import numpy as np
+import datetime
+import math
+import joblib
+
+import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
+import seaborn as sns; sns.set_theme()
+
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import silhouette_score
+
+import warnings
+warnings.filterwarnings('ignore')
+
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).resolve().parent.parent
+DATA_PATH = PROJECT_ROOT / 'data/online retail.xlsx'
+MODELS_DIR = PROJECT_ROOT / 'models'
+PLOTS_DIR = PROJECT_ROOT / 'plots'
+
+MODELS_DIR.mkdir(parents=True, exist_ok=True)
+PLOTS_DIR.mkdir(parents=True, exist_ok=True)
+
+# ===========================
+#  LOADING CUSTOMERS DATASET
+# ===========================
+
+def load_data(filepath):
+    data = pd.read_excel(filepath)
+
+    return data
+
+
+# =========================
+#  EDA
+# =========================
+
+def eda(data):
+    print(f'\n ===== Shape of the dataset ===== \n {data.shape}')
+    print(f'\n =====Dataset Information ===== \n')
+    print(data.info())
+    print(f'\n ===== Checking Missing Values ===== \n {data.isnull().sum().sort_values(ascending=False)}')
+    print(f'\n ===== Summary Statistics ===== \n {data.describe()}')
+
+    id_country = data['Country'].value_counts().reset_index().head(n=10)
+    print(f'\n ===== Index by country ===== \n {id_country}')
+
+    unique_customers = data['CustomerID'].unique().shape
+    percentage_of_orders = (data['CustomerID'].value_counts()/sum(data['CustomerID'].value_counts())*100) .head(n=13).cumsum()
+
+    print(f'\n ===== Amount of Unique Customers ===== \n {unique_customers}')
+    print(f'\n ===== Percentage Of Orders ===== \n {percentage_of_orders}')
+       
+
+
+
+
+
+
+# =========================
+#  MAIN
+# =========================
+
+def main():
+    filepath = DATA_PATH
+    data = load_data(filepath)
+    eda(data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
