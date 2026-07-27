3393dbf4c6c6287324ae4cf7624767aea4aabbd3
codewithraphael <raphaeldaniel223@gmail.com>
2026-07-27 15:47:51 +0100

    add: enhance data processing with feature engineering, data selection, and null value removal functions

--- unsupervised-learning/clustering/clustering-customer-segmentation/src/main.py
+++ unsupervised-learning/clustering/clustering-customer-segmentation/src/main.py
@@ -31,6 +31,7 @@ PLOTS_DIR.mkdir(parents=True, exist_ok=True)
 # ===========================
 
 def load_data(filepath):
+
     data = pd.read_excel(filepath)
 
     return data
@@ -41,13 +42,14 @@ def load_data(filepath):
 # =========================
 
 def eda(data):
+
     print(f'\n ===== Shape of the dataset ===== \n {data.shape}')
     print(f'\n =====Dataset Information ===== \n')
     print(data.info())
     print(f'\n ===== Checking Missing Values ===== \n {data.isnull().sum().sort_values(ascending=False)}')
     print(f'\n ===== Summary Statistics ===== \n {data.describe()}')
 
-    id_country = data['Country'].value_counts().reset_index().head(n=10)
+    id_country = data['Country'].value_counts().reset_index().head(5)
     print(f'\n ===== Index by country ===== \n {id_country}')
 
     unique_customers = data['CustomerID'].unique().shape
@@ -55,9 +57,65 @@ def eda(data):
 
     print(f'\n ===== Amount of Unique Customers ===== \n {unique_customers}')
     print(f'\n ===== Percentage Of Orders ===== \n {percentage_of_orders}')
-       
 
 
+# =========================
+#  FEATURE ENGINEERING
+# =========================
+def feat_eng(data):
+    
+    data['Amount'] = data['Quantity']*data['UnitPrice']
+    print(data)
+
+    return data
+
+# ===========================
+#  DATA CLEANING & SELECTION
+# ===========================
+def select_data(data):
+
+    '''
+    Selecting UK Customers, which are notably the largest segment(based on country)
+    &
+    Removing negative amount or return transactions
+    '''
+    data = data[data.Country == 'United Kingdom']
+
+    data = data[~(data.Amount<0)]
+
+    print(data.head(5))
+    return data
+
+
+# =========================
+#  REMOVING NULL VALUES
+# =========================
+def remove_null(data):
+
+    data = data[~(data.CustomerID.isnull())]
+    print(data.shape)
+
+    return data
+
+
+
+# =========================
+#  RFM MODEL
+# =========================
+def rfm(data):
+
+    '''
+    Recency: The value of how recently a customer purchased at the establishment,
+    Frequency: How frequent the customer’s transactions are at the establishment,
+    Monetary value: The dollar (or pounds in our case) value of all the transactions that the customer made at the establishment
+    '''
+
+    # RECENCY
+    reference_date = data.InvoiceDate.max()
+    reference_date = reference_date + datetime.timedelta(days=1)
+    print()
+
+    pass
 
 
 
@@ -70,7 +128,9 @@ def main():
     filepath = DATA_PATH
     data = load_data(filepath)
     eda(data)
-
+    data = feat_eng(data)
+    data = select_data(data)
+    data = remove_null(data)
 
 
 
