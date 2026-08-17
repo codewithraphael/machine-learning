b8726f12399cded20c7598117db3253b23b51e24
codewithraphael <raphaeldaniel223@gmail.com>
2026-08-18 00:18:16 +0100

    add: create configuration file for project directory structure and file paths

---
+++ time-series-analysis/UR3 CobotOps/src/config.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).resolve().parent.parent
+DATA_DIR = PROJECT_ROOT / "data"
+MODEL_DIR = PROJECT_ROOT / "models"
+PLOTS_DIR = PROJECT_ROOT / "plots"
+
+filepath = DATA_DIR / "UR3_CobotOps.csv"
