ba3066af17c89edcd0a470d366c5a9a90f6bd244
codewithraphael <raphaeldaniel223@gmail.com>
2026-08-18 23:19:56 +0100

    add: implement load_data function to read data from Excel file

--- time-series-analysis/UR3 CobotOps/src/load_data.py
+++ time-series-analysis/UR3 CobotOps/src/load_data.py
@@ -3,6 +3,12 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 import seaborn as sns
+from xgboost import data
 
 from config import filepath, DATA_DIR
 
+
+def load_data(filepath=filepath):
+
+    data = pd.read_excel(filepath)
+    return data
