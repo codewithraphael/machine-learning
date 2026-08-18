64022127ee2ba3bfe3022feadd36b30c24003797
codewithraphael <raphaeldaniel223@gmail.com>
2026-08-18 23:25:33 +0100

    add: restructure main.py to include necessary imports and define main function

--- time-series-analysis/UR3 CobotOps/src/main.py
+++ time-series-analysis/UR3 CobotOps/src/main.py
@@ -1,1 +1,16 @@
+from config import filepath, DATA_DIR
+from load_data import load_data
+from preprocess import preprocess_data
+from train import train_model
+from evaluate import evaluate_model
+
 import warnings; warnings.filterwarnings('ignore')
+
+
+def main():
+    pass
+
+
+
+if __name__ == '__main__':
+    main()
