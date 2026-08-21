995562b0d1247cf4e0af0bdb5795cfdade8f68e4
codewithraphael <raphaeldaniel223@gmail.com>
2026-08-21 23:50:34 +0100

    update: create prepare_temporal_order  function

--- time-series-analysis/UR3 CobotOps/src/feature_engineering.py
+++ time-series-analysis/UR3 CobotOps/src/feature_engineering.py
@@ -1,5 +1,11 @@
-def feature_engineering(data):
+def prepare_temporal_order(data):
 
     '''
-    
+    observations are sorted in temporal order, 
+    so that the model can learn from the past to predict the future.
     '''
+
+    data = data.copy()
+    data = data.sort_values('timestamp').reset_index(drop=True)
+
+    return data
