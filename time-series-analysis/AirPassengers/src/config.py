24cb839ff04889d12c1fb5bd6c1afbfce655e6e6
codewithraphael <raphaeldaniel223@gmail.com>
2026-08-30 21:07:08 +0100

    fix DATA_PATH definition

--- time-series-analysis/AirPassengers/src/config.py
+++ time-series-analysis/AirPassengers/src/config.py
@@ -2,12 +2,8 @@ from pathlib import Path
 
 ROOT = Path(__file__).resolve().parent.parent
 
-DATA_PATH = ROOT / 'data / airline-passengers.csv'
+DATA_PATH = ROOT / 'data' / 'airline-passengers.csv'
 PLOTS_PATH = ROOT / 'plots'
 MODELS_PATH = ROOT / 'models'
-
-DATA_PATH.mkdir(exist_ok=True, parents=True)
-PLOTS_PATH.mkdir(exist_ok=True, parents=True)
-MODELS_PATH.mkdir(exist_ok=True, parents=True)
 
 filepath = DATA_PATH
