d46422cc44d21f338cd0bf282ec13aea992ace40
codewithraphael <raphaeldaniel223@gmail.com>
2026-08-31 04:28:36 +0100

    add plot function call to main for visualizing passenger data

--- time-series-analysis/AirPassengers/src/main.py
+++ time-series-analysis/AirPassengers/src/main.py
@@ -2,6 +2,7 @@ from config import filepath
 from data_loader import load_data
 from eda import eda
 from preprocessing import prepare_datetime, set_time_index
+from plot import plot_time_series
 
 
 def main():
@@ -10,6 +11,7 @@ def main():
     eda(data)
     data = prepare_datetime(data, 'Month')
     data = set_time_index(data, 'Month')
+    plot_time_series(data, 'Passengers')
 
 
 
