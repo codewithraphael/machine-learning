2dee680dfa2deffc1970a8462b3a02dc4c14135d
codewithraphael <raphaeldaniel223@gmail.com>
2026-08-31 03:05:56 +0100

    add preprocessing module for datetime preparation and time indexing

---
+++ time-series-analysis/AirPassengers/src/preprocessing.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+def prepare_datetime(data, date_column):
+
+    data[date_column] = pd.to_datetime(data[date_column])
+
+    return data
+
+
+def set_time_index(data, date_column):
+
+    data = data.set_index(data[date_column])
+
+    print(f'\n ===== INDEXED DATE DATASET ===== \n {data.head(10)}')
+
+    return data
