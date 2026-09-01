b728968f4dcafcf5954d647e0dd14c5f79f4f016
codewithraphael <raphaeldaniel223@gmail.com>
2026-09-01 12:06:43 +0100

    add time series decomposition and ADF test to main workflow

--- time-series-analysis/AirPassengers/src/main.py
+++ time-series-analysis/AirPassengers/src/main.py
@@ -3,6 +3,7 @@ from data_loader import load_data
 from eda import eda
 from preprocessing import prepare_datetime, set_time_index
 from plot import plot_time_series
+from analysis import decompose_time_series, adf_test
 
 
 def main():
@@ -12,6 +13,8 @@ def main():
     data = prepare_datetime(data, 'Month')
     data = set_time_index(data, 'Month')
     plot_time_series(data, 'Passengers')
+    decomposition = decompose_time_series(data, 'Passengers', period=12)
+    adf_test(data['Passengers'])
 
 
 
