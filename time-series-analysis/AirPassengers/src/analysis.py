f9576547920df9e4cd6e14c259cb3147d4e4ac11
codewithraphael <raphaeldaniel223@gmail.com>
2026-09-02 12:39:15 +0100

    refactor analysis.py for improved readability and structure; add differencing function

--- time-series-analysis/AirPassengers/src/analysis.py
+++ time-series-analysis/AirPassengers/src/analysis.py
@@ -2,6 +2,8 @@ from statsmodels.tsa.seasonal import seasonal_decompose
 from statsmodels.tsa.stattools import adfuller
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 
+from config import target_column
+
 import matplotlib.pyplot as plt
 import seaborn as sns; sns.set_theme()
 from config import PLOTS_PATH
@@ -9,7 +11,7 @@ from config import PLOTS_PATH
 
 def decompose_time_series(data, target_column, period):
 
-    plt.figure(figsize=(16, 8))
+    plt.Figure(figsize=(20, 8))
 
     decomposition = seasonal_decompose(
         data[target_column],
@@ -30,16 +32,16 @@ def adf_test(series):
     result = adfuller(series.dropna())
 
     statistic = result[0]
-    p_value = result[1]
+    p_value = result[1]   
 
     print('='*50)
-    print('                ' + 'ADF Statistics')
+    print(' '*16 + 'ADF Statistics')
     print('='*50)
     print(f'\n{statistic:.4f}')
 
 
     print('='*50)
-    print('                 ' + 'P - Value')
+    print(' '*17 + 'P - Value')
     print('='*50)
     print(f'\n{p_value:.4f}')
 
@@ -47,3 +49,22 @@ def adf_test(series):
         print('The series is likely stationary')
     else:
         print('The series is likely non stationary')
+
+
+
+def diff_time_series(data, target_column):
+
+    print('='*90)
+    print(' '*30 + 'DIFFERENCING')
+    print('='*90)
+
+    data['Passengers_diff'] = data[target_column].diff()
+    adf_test(data['Passengers_diff'])
+
+    data['Passengers_diff'].plot(figsize=(15, 5))
+    plt.title('Differenced Passenger Series')
+    plt.xlabel('Date')
+    plt.ylabel('Difference')
+
+    plt.savefig(PLOTS_PATH / 'passenger_differencing.png')
+    plt.close()
