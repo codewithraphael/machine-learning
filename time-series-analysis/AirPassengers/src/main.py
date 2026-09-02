03a1cbfc2390b484c9c0b403cba043707e7280c2
codewithraphael <raphaeldaniel223@gmail.com>
2026-09-02 12:38:33 +0100

    add time series differencing and train-test split to main workflow

--- time-series-analysis/AirPassengers/src/main.py
+++ time-series-analysis/AirPassengers/src/main.py
@@ -1,9 +1,10 @@
-from config import filepath
+from config import filepath, target_column, test_size
 from data_loader import load_data
 from eda import eda
 from preprocessing import prepare_datetime, set_time_index
 from plot import plot_time_series
-from analysis import decompose_time_series, adf_test
+from analysis import decompose_time_series, adf_test, diff_time_series
+from forecasting import train_test_split
 
 
 def main():
@@ -15,6 +16,9 @@ def main():
     plot_time_series(data, 'Passengers')
     decomposition = decompose_time_series(data, 'Passengers', period=12)
     adf_test(data['Passengers'])
+    diff_time_series(data, target_column)
+    train, test = train_test_split(data, target_column, test_size)
+
 
 
 
