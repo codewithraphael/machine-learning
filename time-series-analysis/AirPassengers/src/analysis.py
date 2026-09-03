ebc391feb0c36cc2e7ce7d1ed73e1b84130e9dc2
codewithraphael <raphaeldaniel223@gmail.com>
2026-09-03 13:32:26 +0100

    remove unused imports for ACF and PACF plots in analysis.py

--- time-series-analysis/AirPassengers/src/analysis.py
+++ time-series-analysis/AirPassengers/src/analysis.py
@@ -1,6 +1,6 @@
 from statsmodels.tsa.seasonal import seasonal_decompose
 from statsmodels.tsa.stattools import adfuller
-from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+
 
 from config import target_column
 
