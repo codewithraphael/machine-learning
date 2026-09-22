8b97bdc2832c41a2ed9f712dfcf24ed2660665d2
codewithraphael <raphaeldaniel223@gmail.com>
2026-09-23 00:37:24 +0100

    add: update logistic regression and random forest model parameters for improved training stability

--- supervised-learning/classification/financial risk assessment/src/train.py
+++ supervised-learning/classification/financial risk assessment/src/train.py
@@ -4,12 +4,15 @@ from sklearn.tree import DecisionTreeClassifier
 
 from xgboost import XGBClassifier
 
+import warnings
+warnings.filterwarnings("ignore")
+
 def train_model(X_train, y_train):
 
     models = {
-        'LOGISTIC REGRESSION': LogisticRegression(max_iter=1000),
+        'LOGISTIC REGRESSION': LogisticRegression(max_iter=500, class_weight='balanced', random_state=42),
         'DECISION TREE': DecisionTreeClassifier(random_state=42),
-        'RANDOM FOREST': RandomForestClassifier(random_state=42),
+        'RANDOM FOREST': RandomForestClassifier(random_state=42, class_weight='balanced'),
         'XGBOOST': XGBClassifier(random_state=42)
     }
 
