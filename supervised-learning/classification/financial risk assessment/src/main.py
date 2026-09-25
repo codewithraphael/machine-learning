71f0ed928cd5d27becef428f5c6501852414ddf1
codewithraphael <raphaeldaniel223@gmail.com>
2026-09-26 00:17:36 +0100

    add: store evaluation results and save the best model after training

--- supervised-learning/classification/financial risk assessment/src/main.py
+++ supervised-learning/classification/financial risk assessment/src/main.py
@@ -5,6 +5,7 @@ from data_cleaning import clean_data
 from preprocessing import feature_selection, preprocess_data
 from train import train_model
 from evaluation import evaluate_model, plot_model_comparison
+from utils import save_best_model
 
 import warnings
 warnings.filterwarnings("ignore")
@@ -20,10 +21,14 @@ def main():
     transformer, X_train, X_test = preprocess_data(X_train, X_test)
     trained_models = train_model(X_train, y_train)
 
+    evaluation_results = []
     for name, model in trained_models.items():
-        evaluate_model(name, model, X_train, X_test, y_train, y_test)
+        evaluation_results.append(
+            evaluate_model(name, model, X_train, X_test, y_train, y_test)
+        )
 
     plot_model_comparison(trained_models, X_test, y_test)
+    save_best_model(trained_models, evaluation_results)
 
 
 
